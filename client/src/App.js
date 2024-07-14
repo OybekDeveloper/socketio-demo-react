241d7c170c0396e44daea70d635ee8173a8674ad
Oybek Baxtiyorov <programmeroybek211@gmail.com>
2024-07-15 00:59:28 +0500

    blaa

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import io from "socket.io-client";
 import logo from "./logo.svg";
 import notificationSound from "./sound/notification-sound.mp3";
-const socket = io.connect("http://localhost:4000");
+const socket = io.connect("https://socketio-demo-react.onrender.com");
 function App() {
   const [message, setMessage] = useState("");
   const [room, setRoom] = useState("");
